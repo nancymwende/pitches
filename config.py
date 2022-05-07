5b330ed356e61d521cfa6b6a960bc39ada08b073
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-08 02:44:42 +0300

    ft:initialise the db URI

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
 class Config:
-   
-   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:nancy1234@localhost/pitches'
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:nancy1234@localhost/pitches'
 
 class ProdConfig(Config):
-    pass
-
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
 
 class DevConfig(Config):
     DEBUG = True
