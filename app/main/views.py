620fad2a7070858e3a1b33b967bbaf8eee278e00
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-08 16:50:08 +0300

    ft:generate the form file

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,13 @@
 from flask import render_template
 from .import main
+from ..models import User, Upvote,Downvote,Comment 
+from flask_login import login_required
 
 @main.route('/')
 def index():
 
     return render_template('index.html')
     
+    
+    
+# @main.route('/movie/renew/new/int<:id>', methods = ['GET','POST'])
--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+   return User.query.get(int(user_id))
+
 
 class Post(db.Model):
    __tablename__ = 'Posts'
@@ -9,19 +15,15 @@ class Post(db.Model):
    category = db.Column(db.String)
    content = db.Column(db.Text)
    date_posted = db.Column(db.DateTime,default = datetime.utcnow)
-   
-   
-   
-   
-   
-   
-   
+
+
+
 class User(db.Model):
    __tablename__ = 'users'
    id = db.Column(db.Integer,primary_key = True)
    username = db.Column(db.String(255))
-   
-   
+   email = db.Column(db.String(255), unique = True,index = True)
+   password_hash =db.Column(db.String(255))
    
    @property
    def password(self):
@@ -33,6 +35,8 @@ class User(db.Model):
    
    def verify_password(self,password):
       return check_password_hash(self.pass_secure, password)
+      
+      
    
    
    
