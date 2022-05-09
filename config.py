1adafc7691329118b93e62b8f695d38df8d1abe3
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-10 01:24:08 +0300

    ft:create email configurations for user email

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:nancy1234@localhost/pitches'
-
+    SECRET_KEY = 'slihbvsdvhjbhnhlmjknhjbgy'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
 
