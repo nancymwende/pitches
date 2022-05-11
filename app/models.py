8061533d11e583a0706587b9372571d4d4e00e18
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-11 10:31:54 +0300

    refactor:change Posts table to posts

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
+from flask_login import UserMixin
 
 
 @login_manager.user_loader
@@ -9,21 +10,25 @@ def load_user(user_id):
 
 
 class Post(db.Model):
-   __tablename__ = 'Posts'
+   __tablename__ = 'posts'
    id = db.Column(db.Integer,primary_key = True)
    title = db.Column(db.String(255))
    category = db.Column(db.String)
    content = db.Column(db.Text)
+   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
    date_posted = db.Column(db.DateTime,default = datetime.utcnow)
 
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
    __tablename__ = 'users'
    id = db.Column(db.Integer,primary_key = True)
    username = db.Column(db.String(255))
    email = db.Column(db.String(255), unique = True,index = True)
-   password_hash =db.Column(db.String(255))
+   bio = db.Column(db.String(255))
+   profile_pic_path = db.Column(db.String())
+   password_hash = db.Column(db.String(255))
+   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+   posts = db.relationship('Post',backref = 'posts',lazy = 'dynamic')
    
    @property
    def password(self):
@@ -31,18 +36,10 @@ class User(db.Model):
       
    @password.setter
    def password(self,password):
-      self.pass_secure = generate_password_hash(password)
+      self.password_hash = generate_password_hash(password)
    
    def verify_password(self,password):
-      return check_password_hash(self.pass_secure, password)
-      
-      
-   
-   
-   
-   
-   
-   
+      return check_password_hash(self.password_hash, password)
    
 class Upvote(db.Model):
    __tablename__ = 'upvotes'
@@ -50,23 +47,11 @@ class Upvote(db.Model):
    user_id = db.Column(db.Integer)
    
    
-   
-   
-   
-   
-   
-   
+
 class Downvote(db.Model):
    __tablename__ = 'downvotes'
    id = db.Column(db.Integer,primary_key = True)
    user_id = db.Column(db.Integer)
-   
-   
-   
-   
-   
-   
-   
    
 class Comment(db.Model):
    __tablename__ = 'comments'
