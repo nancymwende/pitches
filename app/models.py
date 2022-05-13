d6c9d9b8233478e0f39ccbf502f9e8882fccbbb9
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-13 21:53:09 +0300

    ft:relationship between the table and class user

--- app/models.py
+++ app/models.py
@@ -17,6 +17,13 @@ class Post(db.Model):
    content = db.Column(db.Text)
    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
    date_posted = db.Column(db.DateTime,default = datetime.utcnow)
+   upvotes = db.relationship('Upvote',backref = 'posts',lazy = 'dynamic')
+   downvotes = db.relationship('Downvote',backref = 'posts',lazy = 'dynamic')
+   
+   
+   def save_post(self):
+      db.session.add(self)
+      db.session.commit()
 
 
 class User(UserMixin,db.Model):
@@ -28,7 +35,9 @@ class User(UserMixin,db.Model):
    profile_pic_path = db.Column(db.String())
    password_hash = db.Column(db.String(255))
    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-   posts = db.relationship('Post',backref = 'posts',lazy = 'dynamic')
+   posts = db.relationship('Post',backref = 'post',lazy = 'dynamic')
+   upvotes = db.relationship('Upvote',backref = 'post',lazy = 'dynamic')
+   downvotes = db.relationship('Downvote',backref = 'post',lazy = 'dynamic')
    
    @property
    def password(self):
@@ -44,20 +53,48 @@ class User(UserMixin,db.Model):
 class Upvote(db.Model):
    __tablename__ = 'upvotes'
    id = db.Column(db.Integer,primary_key = True)
-   user_id = db.Column(db.Integer)
+   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+   post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+   
+   
+   def save(self):
+      db.session.add(self)
+      db.session.commit()
+   @classmethod
+   def get_upvotes(cls,id):
+      upvotes = Upvote.query.filter_by(post_id=id).all()
+      return upvotes
+      
+      
+   def _repr_(self):
+      return f'{self.user_id}:{self.post_id}'
    
    
 
 class Downvote(db.Model):
    __tablename__ = 'downvotes'
    id = db.Column(db.Integer,primary_key = True)
-   user_id = db.Column(db.Integer)
+   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+   post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+   
+   def save(self):
+      db.session.add(self)
+      db.session.commit()
+   @classmethod
+   def get_downvotes(cls,id):
+      downvotes = Downvote.query.filter_by(post_id=id).all()
+      return downvotes
+      
+      
+   def _repr_(self):
+      return f'{self.user_id}:{self.post_id}'
    
 class Comment(db.Model):
    __tablename__ = 'comments'
    id = db.Column(db.Integer, primary_key=True)
    comment = db.Column(db.String(255))
-   user_id = db.Column(db.Integer)
+   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+   post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
    
    
    
