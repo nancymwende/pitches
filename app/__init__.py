d71507c232587038c0860e9538b0182b1e0d20fa
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-13 21:57:52 +0300

    fix:register blueprint. (i)change the import fromauth to main.

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,21 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+
+bootstrap = Bootstrap()
 db = SQLAlchemy()
-bootstrap = Bootstrap
-
+mail = Mail()
 login_manager = LoginManager()
+filestorage = FileStorage
+
+
 login_manager.session_protection = 'strong'
-login_manager . login_view = 'auth/login'
-
-
-
-
+login_manager . login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
 
 def create_app(config_name):
      app = Flask(__name__)
@@ -21,13 +26,21 @@ def create_app(config_name):
      app.config.from_object(config_options[config_name])
      
      # Initializing Flask Extensions
-     bootstrap = Bootstrap(app)
+     bootstrap.init_app(app)
      db.init_app(app)
      login_manager.init_app(app)
-     
+     mail.init_app(app)
      
      #registering blueprint
      from .main import main as main_blueprint
      app.register_blueprint(main_blueprint)
+
+
+     from .auth import auth as auth_blueprint
+     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+     
+     configure_uploads(app, photos)
      
      return app
+     
+     
